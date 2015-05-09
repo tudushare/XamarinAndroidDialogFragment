20b7f9536c378847f80f7c0ed7bbdbea22559d02
Vu Dang Tuan <vdtuan.cse10@gmail.com>
2015-05-09 10:01:13 +0700

    Add project

---
+++ TDDialogDemo/TDDialogDemo/MainActivity.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace TDDialogDemo
+{
+	[Activity (Label = "TDDialogDemo", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : Activity
+	{
+		
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+
+			// Get our button from the layout resource,
+			// and attach an event to it
+			Button button = FindViewById<Button> (Resource.Id.btnShowDialog);
+			
+			button.Click += delegate {
+				// show the dialog
+				FragmentTransaction transaction = FragmentManager.BeginTransaction();
+				TDDialog dialog = new TDDialog();
+				dialog.Show(transaction, "dialog fragment");
+			};
+		}
+	}
+}
+
+
---
+++ TDDialogDemo/TDDialogDemo/TDDialog.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Android.App;
+using Android.Widget;
+
+namespace TDDialogDemo
+{
+	public class TDDialog: DialogFragment
+	{
+		public override Android.Views.View OnCreateView (Android.Views.LayoutInflater inflater, Android.Views.ViewGroup container, Android.OS.Bundle savedInstanceState)
+		{
+			base.OnCreateView (inflater, container, savedInstanceState);
+			var view = inflater.Inflate (Resource.Layout.tdDialogLayout, container, false);
+			var btnCloseDialog = view.FindViewById<Button> (Resource.Id.btnCloseDialog);
+			btnCloseDialog.Click += (object sender, EventArgs e) => {
+				Dismiss();
+			};
+			return view;
+		}
+
+		public override void OnActivityCreated (Android.OS.Bundle savedInstanceState)
+		{
+			Dialog.Window.RequestFeature (Android.Views.WindowFeatures.NoTitle); // hide title bar
+			base.OnActivityCreated (savedInstanceState);
+			Dialog.Window.Attributes.WindowAnimations = Resource.Style.dialog_animation; // add animation
+		}
+	}
+}
+
